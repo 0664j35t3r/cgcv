8d79e8f46449785173fe7059355010555c8ba23d
Peter Lorenz <0664jester@users.noreply.github.com>
2015-05-10 15:42:05 +0200

    Update task1b.cpp

--- cg/task1b/task1b.cpp
+++ cg/task1b/task1b.cpp
@@ -1,4 +1,5 @@
 #include <complex>
+#include <opencv2/core/types_c.h>
 
 #include "RayTracer1b.h"
 #include "Scene1b.h"
@@ -25,22 +26,13 @@ void computeSurfaceParameters(const Triangle* triangle, const float2& barycentri
 
   // For now, we just set the surface normal to the normal of the triangle and
   // the texture coordinates to the barycentric coordinates of the hitpoint.
-  n = triangle->triangleNormal;
-  texCoords = barycentric;
+  n  = (1 - barycentric.x - barycentric.y) * triangle->normal1 + 
+    barycentric.x * triangle->normal2 + 
+    barycentric.y * triangle->normal3;
   
-  math::float3 n1 = triangle->normal1;
-  math::float3 n2 = triangle->normal2;
-  math::float3 n3 = triangle->normal3;
-  
-  math::float2 t1 = triangle->texture1;
-  math::float2 t2 = triangle->texture2;
-  math::float2 t3 = triangle->texture3;
-  
-  n  = (1 - barycentric.x - barycentric.y) * n1 + barycentric.x * n2 + barycentric.y * n3;
-  n.normalize();
-  
-  texCoords = (1 - barycentric.x - barycentric.y) * t1 + barycentric.x * t2 + barycentric.y * t3;
-  
+  texCoords = (1 - barycentric.x - barycentric.y) * triangle->texture1 + 
+    barycentric.x * triangle->texture2 + 
+    barycentric.y * triangle->texture3;
 }
 
 float4 sampleTexture(const float2& texCoords, const surface<R8G8B8A8>& texture)
@@ -57,54 +49,38 @@ float4 sampleTexture(const float2& texCoords, const surface<R8G8B8A8>& texture)
 // access the texture image with
 //    texture(x, y).toFloat4()
 //
-  
   float x = texCoords.x;
   float y = texCoords.y;
   
+  int px = (int)(x * texture.width());
+  int py = (int)(y * texture.height());
+    
+  R8G8B8A8 c_11 = texture(px    , py    );
+  R8G8B8A8 c_12 = texture(px + 1, py    );
+  R8G8B8A8 c_21 = texture(px    , py + 1);
+  R8G8B8A8 c_22 = texture(px + 1, py + 1);
   
-  int xi = (int)(x * (float)texture.width());
-  int yi = (int)(y * (float)texture.height());
+  float intersec = x * (float)texture.width() - (float)px;
   
-  int x11 = xi;
-  int y11 = yi;
-  
-  int x12 = xi + 1;
-  int y12 = yi;
-  
-  int x21 = xi;
-  int y21 = yi + 1;
-  
-  int x22 = xi + 1;
-  int y22 = yi + 1;
-  
-  R8G8B8A8 c11 = texture(x11, y11);
-  R8G8B8A8 c12 = texture(x12, y12);
-  R8G8B8A8 c21 = texture(x21, y21);
-  R8G8B8A8 c22 = texture(x22, y22);
-  
-  
-  float intersec = x * (float)texture.width() - (float)x11;
-  
-  R8G8B8A8 obn = R8G8B8A8(  intersec * c11.r() + (1 - intersec) * c12.r(),
-                            intersec * c11.g() + (1 - intersec) * c12.g(),
-                            intersec * c11.b() + (1 - intersec) * c12.b(),
-                            intersec * c11.a() + (1 - intersec) * c12.a() ); 
+  R8G8B8A8 obn = R8G8B8A8( intersec * c_11.r() + (1 - intersec) * c_12.r(),
+                           intersec * c_11.g() + (1 - intersec) * c_12.g(),
+                           intersec * c_11.b() + (1 - intersec) * c_12.b(),
+                           intersec * c_11.a() + (1 - intersec) * c_12.a() ); 
   
 
-  R8G8B8A8 untn = R8G8B8A8( intersec * c21.r() + (1 - intersec) * c22.r(),
-                            intersec * c21.g() + (1 - intersec) * c22.g(),
-                            intersec * c21.b() + (1 - intersec) * c22.b(),
-                            intersec * c21.a() + (1 - intersec) * c22.a() );
+  R8G8B8A8 untn = R8G8B8A8( intersec * c_21.r() + (1 - intersec) * c_22.r(),
+                            intersec * c_21.g() + (1 - intersec) * c_22.g(),
+                            intersec * c_21.b() + (1 - intersec) * c_22.b(),
+                            intersec * c_21.a() + (1 - intersec) * c_22.a() );
 
   
-  intersec = y * (float)texture.height() - (float)y11;
+  intersec = y * (float)texture.height() - (float)py;
   
   R8G8B8A8 interpolated = R8G8B8A8( intersec * obn.r() + (1 - intersec) * untn.r(),
                                     intersec * obn.g() + (1 - intersec) * untn.g(),
                                     intersec * obn.b() + (1 - intersec) * untn.b(),
                                     intersec * obn.a() + (1 - intersec) * untn.a() ); 
   
-  // For now, just return the top left texel.
   return interpolated.toFloat4();
 }
 
@@ -117,22 +93,23 @@ float3 phong(const float3& p, const float3& n, const float3& v, const float3& k_
 //
   // calculate the angle theta
   // calculate c_d
-  float3 vector_l = float3(lightPos - p).normalize();
   // https://www.ltcconline.net/greenl/courses/107/vectors/dotcros.htm  
+  float3 vector_l = normalize(static_cast<float3>(lightPos - p));
+  float3 norm = normalize(n);
   float theta = dot(n, vector_l);
-    
-  float3 c_d = k_d * theta;//cos(theta);
+  
+  float3 c_d = k_d * MAX(theta, 0);
   
   // calculate c_s
-  //                                              r = 2n(n l)  - l , 3. foliensatz s54
-  float3 vector_r = float3( 2 * n * cross(n, vector_l) - vector_l).normalize();
-  float3 v_norm = v;
-  float alpha = dot(v_norm.normalize(), vector_r.normalize());
+  //                                  r = 2n(n l)  - l , 3. foliensatz s54
+  float3 r = float3(2 * norm * cross(norm, vector_l) - vector_l);
+  //  float alpha = dot(normalize(v_norm), normalize(vector_r));
+  float alpha = dot(normalize(v), normalize(r));
   
   float3 c_s(0,0,0);
   if(theta)
-    c_s = k_s * pow(alpha, shininess);
-  
+    c_s = k_s * pow(MAX(alpha, 0), shininess);
+    
   return (c_s + c_d) * lightColor;
 }
 
@@ -147,16 +124,15 @@ bool inShadow(const float3& p, const float3& lightPos, const Scene1b& scene)
 // of the scene object to compute whether a ray intersects the scene
 // geometry.
 //
-
-  // example intersection test
   const Triangle* intersectedTriangle;
-  Ray ray(lightPos, p - lightPos);
+  Ray ray(p, lightPos - p);
   float t;
-  // For now, just say there is no shadow.
   return scene.intersectWithRay(intersectedTriangle, t, ray);
+//  return false;
 }
 
-float3 shade(const Scene1b& scene, const float3& p, const float3& n, const float3& v, const float3& k_d, const float3& k_s, float shininess, int bounce, int max_bounce)
+float3 shade(const Scene1b& scene, const float3& p, const float3& n, const float3& v, 
+  const float3& k_d, const float3& k_s, float shininess, int bounce, int max_bounce)
 {
 //TODO:
 // Compute the shading of the given surface point with the given material
@@ -169,11 +145,18 @@ float3 shade(const Scene1b& scene, const float3& p, const float3& n, const float
 //    std::isinf(shininess)
 //
   // float3 phong(const float3& p, const float3& n, const float3& v, const float3& k_d, const float3& k_s, float shininess, const float3& lightPos, const float3& lightColor)
-  
   float3 result(0,0,0);
-  std::vector<Pointlight> vec_color = scene.getLightList();
-  for(auto &light : vec_color)
-    result += phong(p, n, v, k_d, k_s, std::isinf(shininess), light.pos, light.color);
+
+  if(std::isinf(shininess))
+  {
+    return float3(1.0f ,1.0f ,1.0f);
+  }
+  
+  for(auto& light : scene.getLightList())
+  {
+    if(!inShadow(p, light.pos, scene))
+      result += phong(p, n, v, k_d, k_s, shininess, light.pos, light.color);
+  }
   
   return result;
 }
@@ -206,21 +189,19 @@ float3 traceRay(const Scene1b& scene, const Ray& ray, int bounce, int max_bounce
 //    tri->materialIndex
 //
   static const float epsilon = 0.0001f;
-  
-  const Triangle* tri;
-  float2 texcords;
+  const Triangle* triangle;
   float t;
+  float2 barycentric;
+  float3 n;
+  float2 texCoords;
   
-//  tri->triangleNormal + epsilon;
-  if(!scene.intersectWithRay(tri, t, ray))
+  bool intersect = scene.intersectWithRay(triangle, t, ray, barycentric);
+  if (!intersect)
     return scene.background();
   
-  PhongMaterial material = scene.materialParameters(tri->materialIndex, texcords);
-//  material.k_d
-//  material.k_s
-//  material.shininess
-//  
-
-
-  // For now, just return the background color.
+  computeSurfaceParameters(triangle, barycentric, n, texCoords);
+  PhongMaterial material = scene.materialParameters(triangle->materialIndex, texCoords);
+  
+  return shade(scene, ray.origin + ray.direction * t + triangle->triangleNormal * epsilon, 
+    n, -ray.direction, material.k_d, material.k_s, material.shininess, bounce, max_bounce); 
 }
